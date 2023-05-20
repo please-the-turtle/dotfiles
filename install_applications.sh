d5938494fc56c94028b07854717a68e11efd4eed
Alexey Viklov <alexey.viklov@gmail.com>
2023-05-20 23:30:09 +0300

    install_applications.sh script updated.

--- install_applications.sh
+++ install_applications.sh
@@ -2,16 +2,17 @@
 
 yay -S --needed \
   wget unzip neofetch \
-	firefox btop \
-	libreoffice-still neovim \
-	thunar \
-	obsidian \
-	telegram-desktop thunderbird \
-	pavucontrol \
-	wdisplays \
-	starship \
-	thunar-archive-plugin \
-	udisks2 \
-	viewnior \
-	easyeffects calf lsp-plugins \
-	udisks2 udiskie \
+  firefox btop \
+  libreoffice-still neovim \
+  thunar \
+  obsidian \
+  telegram-desktop thunderbird \
+  pavucontrol \
+  wdisplays \
+  starship \
+  thunar-archive-plugin \
+  udisks2 \
+  viewnior \
+  easyeffects calf lsp-plugins \
+  udisks2 udiskie \
+  tty-clock-git \
